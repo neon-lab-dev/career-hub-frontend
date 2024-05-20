e784c8c96250f85e7d0a898c6cb0729cdacf34d3
rahulsd380 <rahulsd380@gmail.com>
2024-05-20 23:09:57 +0600

    Project setup

---
+++ src/app/(pages)/contact-us/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            This is the contact us page.
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/(pages)/short-term-courses/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            This is from Short-Term-Courses
+        </div>
+    );
+};
+
+export default page;
