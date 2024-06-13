58c28fc653298ac3ed1140433f7a0077b593854e
jrtilak <iamjrtilak@gmail.com>
2024-06-13 21:20:32 +0545

    chore: add routes for the employees

---
+++ src/app/(employee)/(job-listing)/[jobType]/page.tsx
@@ -0,0 +1,14 @@
+import NotFound from "@/components/NotFound";
+
+type Props = {
+  params: {
+    jobType: string;
+  };
+};
+
+const AVAILABLE_JOB_TYPES = ["internships", "jobs"];
+
+export default function Page({ params: { jobType } }: Props) {
+  if (!AVAILABLE_JOB_TYPES.includes(jobType)) return <NotFound />;
+  if (jobType) return <div>My Post: {jobType}</div>;
+}
