b03d30eb1a6ce9dffa0b8175bf0a24766c8a38ca
Premanand Chowdhury <freudigprem@gmail.com>
2024-06-17 16:27:27 +0530

    chore: KPI Card

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,15 @@
 import companyLogo from "@/assets/images/company-logo.png";
 
+// ICONS
+import hourGlass from '@/assets/icons/hourglass.svg';
+import correctPrimary from '@/assets/icons/correct-primary.svg';
+import crossPrimary from '@/assets/icons/correct-primary.svg';
+import application from '@/assets/icons/applications.svg';
+
 export const IMAGES = {
   companyLogo,
+  hourGlass,
+  correctPrimary,
+  crossPrimary,
+  application,
 };
