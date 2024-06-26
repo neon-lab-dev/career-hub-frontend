dc033d3bb29408f6665eac9c5025e5a57b9787c1
MekalaArunKumar16 <mekalaarunkumar2002@gmail.com>
2024-06-26 16:02:41 +0530

    created getting-started for employer

--- src/app/(employee)/getting-started/_components/getStartedLayout.tsx
+++ src/app/(employee)/getting-started/_components/getStartedLayout.tsx
@@ -9,11 +9,11 @@ interface GetStartedLayoutProps {
 }
 const GetStartedLayout: React.FC<GetStartedLayoutProps> = ({ children, progress, goToPreviousStep }) => {
   return (
-    <div className="bg-[#F5F6FA] w-full p-6">
-      <div className="bg-white pt-10 mb-10 rounded-2xl mx-32 h-[600px]">
-        <div className="flex justify-center items-center gap-4">
-          <Image src={IMAGES.arrow} alt="" onClick={goToPreviousStep} />
-          <progress className="progress progress-error w-[700px] h-[15px] transition-transform duration-300" value={progress} max="100"></progress>
+    <div className="bg-[#F5F6FA] w-full p-6 max-sm:p-1">
+      <div className="bg-white pt-10 mb-10 rounded-2xl mx-32 max-lg:mx-2 max-md:mx-0.8 h-[600px] max-lg:h-full">
+        <div className="flex justify-center items-center gap-4 max-lg:gap-0 max-md:gap-1 max-lg:mx-6 max-md:mx-4">
+          <Image src={IMAGES.arrow} alt="" onClick={goToPreviousStep} className=' max-md:w-6 ' />
+          <progress className="progress progress-error w-[700px]  h-[15px] transition-transform duration-300 " value={progress} max="100"></progress>
         </div>
         {children}
       </div>
