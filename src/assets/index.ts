2503024854adebe61bc866d4840abb77450a4155
MekalaArunKumar16 <mekalaarunkumar2002@gmail.com>
2024-06-27 22:19:31 +0530

    fix:deployment erros

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -25,16 +25,7 @@ import download from "@/assets/images/fi-br-download (1).svg"
 import penResume from "@/assets/images/Pen.svg"
 import penEdit from "@/assets/images/Vector.svg"
 
-import arrow from "@/assets/icons/arrow_back.svg";
-import pen from "@/assets/icons/Pen.svg";
-import bin from "@/assets/icons/Trash Bin Trash.svg";
-import circle from "@/assets/icons/Add Circle.svg";
-import sucess from "@/assets/icons/image 9.svg";
-import papperclip from "@/assets/icons/Paperclip.svg";
-import hamburger from "@/assets/images/Hamburger Menu.svg";
-import close from "@/assets/icons/Close Circle.svg";
-import profile from "@/assets/images/Ellipse 169.svg";
-import down from "@/assets/icons/Alt Arrow Down.svg";
+
 
 export const IMAGES = {
   companyLogo,
@@ -52,7 +43,7 @@ export const IMAGES = {
   close,
   profile,
   down,
-  download
+  download,
   hero1,
   hero2,
   hero3,
