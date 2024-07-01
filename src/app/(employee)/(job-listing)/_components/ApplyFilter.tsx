880f0c288968cd695ae71cfc35aea34cbe821f68
Rahul Sutradhar <rahulsd380@gmail.com>
2024-07-01 20:37:28 -0300

    test

--- src/app/(employee)/(job-listing)/_components/ApplyFilter.tsx
+++ src/app/(employee)/(job-listing)/_components/ApplyFilter.tsx
@@ -31,7 +31,7 @@ const ApplyFilter = () => {
       <div className="flex flex-col gap-8">
         <div className="flex items-center justify-between">
           <h1 className="text-neutral-900 text-[18px] font-600">
-            {/* Apply Filters */}
+            Apply Filters
           </h1>
 
           <button
