b92f9f8cc6ca90cdcae92bbdd489dcae13dd85b4
Rahul Sutradhar <rahulsd380@gmail.com>
2024-07-03 21:00:57 -0300

    auth route added

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,21 @@ const baseUrl = process.env.BASE_URL + '/api/v1';
 const api = {
     jobs : baseUrl + '/jobs',
     createJob : baseUrl + '/createjob',
+
+    // for employees
+    employeeRegister : baseUrl + '/register',
+    employeeLogin  : baseUrl + '/login',
+    employeeOTPVerify : baseUrl + '/verify',
+    employeeLogout : baseUrl + '/employee/logout',
+    employeeForgotPassword : baseUrl + '/password/forgot',
+    employeeChangePassword : baseUrl + '/password/update',
+    employeeResetPassword: baseUrl + '/password/reset',
+    employeeEnterDetails : baseUrl + '/user/details',
+    employeeProfile : baseUrl + '/me',
+    updateEmployeeProfile : baseUrl + '/me/update',
+    employeeUploadResume : baseUrl + '/resumes',
+
+    // for employers
     employerJob : baseUrl + '/employeer/job',
     employerRegistration : baseUrl + '/register/employeer',
     employerOTPVerify : baseUrl + '/verify/employeer',
