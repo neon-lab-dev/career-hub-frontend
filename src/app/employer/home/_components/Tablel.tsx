6c3b3d968f7c2ab1e27f2fc4f7db82b8ca4813ad
MekalaArunKumar16 <mekalaarunkumar2002@gmail.com>
2024-07-09 10:58:39 +0530

    feat:updated empolyee dashboard

---
+++ src/app/employer/home/_components/Tablel.tsx
@@ -0,0 +1,117 @@
+"use client"
+import { ICONS, IMAGES } from '@/assets';
+import React, { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
+import Image from 'next/image';
+import StatusLabel from "@/components/StatusLabel";
+import Link from 'next/link';
+
+const Table = ({ className }) => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const handleMenuClick = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
+    return (
+        <div className={twMerge(`w-full overflow-x-auto h-[700px] max-w-[1300px] mx-auto px-0 ${className}`)}>
+            <div className="rounded-[124px]">
+                <table className="table w-full">
+                    <thead className="bg-secondary-100 w-full text-secondary-800 font-plus-jakarta-sans font-500">
+                        <tr>
+                            <td>
+                                <div className='flex items-center gap-2 text-lg'>
+                                    <Image src={ICONS.rectangle} alt="Role Icon" />
+                                    <span>Role</span>
+                                </div>
+                            </td>
+                            <td>
+                                <div className='flex items-center gap-2 text-lg'>
+                                    <span>Stipend Offered</span>
+                                </div>
+                            </td>
+                            <td>
+                                <div className='flex items-center gap-2 text-lg'>
+                                    <span>Total Applications</span>
+                                </div>
+                            </td>
+                            <td>
+                                <div className='flex items-center gap-2 text-lg'>
+                                    <span>Job Type</span>
+                                </div>
+                            </td>
+                            <td>
+                                <div className='flex items-center gap-2 text-lg'>
+                                    <span>Status</span>
+                                </div>
+                            </td>
+                            <td>
+                                <div className='flex items-center gap-2 text-lg'>
+                                    <span>Action</span>
+                                </div>
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody className="bg-white w-full">
+                        <tr>
+                            <td>
+                                <div className='flex items-center gap-2 text-lg'>
+                                    <Image src={ICONS.rectangle} alt="Role Icon" />
+                                    <span>Product Designer</span>
+                                </div>
+                            </td>
+                            <td>
+                                <div className='flex items-center gap-2 text-lg'>
+                                    <span>15000</span>
+                                </div>
+                            </td>
+                            <td>
+                                <div className='flex items-center gap-2 text-lg'>
+                                    <span>100 <span className='text-red-500 underline cursor-pointer'>View Applications</span></span>
+                                </div>
+                            </td>
+                            <td>
+                                <div className='flex items-center gap-2 text-lg'>
+                                    <span>Full-Time</span>
+                                </div>
+                            </td>
+                            <td>
+                                <div className='flex items-center gap-2 text-lg'>
+                                    <StatusLabel key="status" variant='applied'>Under Review</StatusLabel>
+                                </div>
+                            </td>
+                            <td>
+                                <div className='relative flex items-center gap-2 text-lg'>
+                                    <div onClick={handleMenuClick} className='cursor-pointer'>
+                                        <Image src={IMAGES.menudots} alt="Menu Icon" />
+                                    </div>
+                                    {dropdownOpen && (
+                                        <div className="absolute right-0 mt-48 w-48 p-4 rounded-xl bg-white border  shadow-lg z-10">
+                                            <Link href="/employer/home/view-applcations" >
+                                                <div className='flex items-center gap-2 text-sm p-2'>
+                                                    <Image src={ICONS.doc} alt="Role Icon" />
+                                                    <span>View Applications</span>
+                                                </div>
+                                            </Link>
+                                            <Link href="/employer/home/view-applcations" >
+                                                <div className='flex items-center gap-2 text-sm p-2'>
+                                                    <Image src={IMAGES.view} alt="Role Icon" />
+                                                    <span>View</span>
+                                                </div>
+                                            </Link>
+                                            <div className='flex items-center gap-2 text-sm p-2'>
+                                                <Image src={IMAGES.bin} alt="Role Icon" />
+                                                <span className=' text-red-500'>delete</span>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+export default Table;
---
+++ src/app/employer/home/add-new-hiring/page.tsx
@@ -0,0 +1,70 @@
+import { ICONS, IMAGES } from '@/assets'
+import Button from '@/components/Button'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import menuDots from '@/assets/icons/menu-dots.svg';
+
+
+const page = () => {
+    return (
+        <div className=' p-6 bg-[#f5f6fa]'>
+            <div className=' bg-white p-6 rounded-xl '>
+                <div className='flex justify-between'>
+                    <div className=' flex gap-6 items-center'>
+                        <Link href="/employer/home" ><Image src={IMAGES.arrow} />
+                        </Link>
+                        <h1 className=' text-neutral-950 text-[28px] font-700'>Add New Hiring</h1>
+                    </div>
+                </div>
+                <div className='flex justify-center mt-16 gap-6'>
+                    <div className='flex flex-col gap-2'>
+                        <label htmlFor=""><span className=' text-lg'>Job Title</span></label>
+                        <input type="text" placeholder='eg., UX Designer' className='p-3 border rounded-xl w-[500px]' />
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <label htmlFor=""><span className=' text-lg'>Job Type</span></label>
+                        <input type="text" placeholder='eg., UX Designer' className='p-3 border rounded-xl w-[250px]' />
+                    </div>
+                </div>
+                <div className='flex justify-center mt-8 gap-6'>
+                    <div className='flex flex-col gap-2'>
+                        <label htmlFor=""><span className=' text-lg'>Roles and Responsibilities - Job Description</span></label>
+                        <textarea type="text" placeholder='eg., UX Designer' className='p-3 border rounded-xl w-[770px]' />
+                    </div>
+                </div>
+                <div className='flex justify-center mt-8 gap-6'>
+                    <div className='flex flex-col gap-2'>
+                        <label htmlFor=""><span className=' text-lg'>From</span></label>
+                        <input type="date" placeholder='' className='p-3 border rounded-xl w-[370px]' />
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <label htmlFor=""><span className=' text-lg'>To</span></label>
+                        <input type="date" placeholder='' className='p-3 border rounded-xl w-[370px]' />
+                    </div>
+                </div>
+                <div className='flex justify-center mt-8 gap-6'>
+                    <div className='flex flex-col gap-2'>
+                        <label htmlFor=""><span className=' text-lg'>Salary</span></label>
+                        <input type="text" placeholder='' className='p-3 border rounded-xl w-[750px]' />
+                    </div>
+                </div>
+                <div className='flex justify-center mt-8 gap-6'>
+                    <div className='flex flex-col gap-2'>
+                        <label htmlFor=""><span className=' text-lg'>Skills Required</span></label>
+                        <input type="text" placeholder='eg., UI, UX, Design' className='p-3 border rounded-xl w-[370px]' />
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <label htmlFor=""><span className=' text-lg'>Extra Benefits</span></label>
+                        <input type="text" placeholder='eg., Hehe' className='p-3 border rounded-xl w-[370px]' />
+                    </div>
+                </div>
+                <div className='flex justify-center w-[600px]'>
+                    <Button variant='primary' className='mt-4' >Submit</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default page
