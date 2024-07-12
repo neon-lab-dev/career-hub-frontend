2c33d10858a8dc7090efdd6d360fdc6b456429cd
jrtilak <iamjrtilak@gmail.com>
2024-07-12 12:38:21 +0545

    fix: header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { IMAGES } from "@/assets";
@@ -31,7 +31,7 @@ const navItems = [
 
 const pfileItems = [
   { text: "My Applications", href: "/applications" },
-  { text: "Edit Resume", href: "" },
+  { text: "Edit Resume", href: "/resume" },
   { text: "Edit Preferences", href: "" },
 ];
 
@@ -67,6 +67,11 @@ const Navbar = () => {
     },
   });
 
+  // close the sidebar on route change
+  useEffect(() => {
+    setIsMobileSidebarOpen(false);
+  }, [pathname]);
+
   return (
     <>
       <div className="flex justify-between items-center  bg-white py-6  max-lg:px-4 wrapper max-width m-auto">
@@ -90,18 +95,12 @@ const Navbar = () => {
             // student route
             <>
               {studentProfile ? (
-                <div className="dropdown dropdown-bottom dropdown-end dropdown-hover">
+                <div className="dropdown dropdown-bottom dropdown-end">
                   <div tabIndex={0} role="button" className="  flex gap-1">
                     <div className="size-10 rounded-full border border-primary-500 text-primary-500 font-bold flex justify-center items-center">
                       <p>{studentProfile.full_name[0]}</p>
                     </div>
                     {/* <Image src={IMAGES.profile} alt={""} /> */}
-
-                    <Image
-                      src={IMAGES.down}
-                      alt={""}
-                      className=" hover:rotate-180 transition-transform duration-00"
-                    />
                   </div>
                   <ul
                     tabIndex={0}
@@ -114,6 +113,21 @@ const Navbar = () => {
                       <span>{studentProfile.email}</span>
                     </div>
                     <hr />
+                    {/* profile links */}
+                    {
+                      <ul className="text-base text-neutral-600 font-medium">
+                        {pfileItems.map((item, index) => (
+                          <li
+                            key={index}
+                            className="hover:text-primary-500 py-0.5 font-bold"
+                          >
+                            <Link href={item.href}>
+                              <div>{item.text}</div>
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    }
                     <hr />
                     <div className="flex justify-center">
                       <button
@@ -228,7 +242,16 @@ const Navbar = () => {
         </div>
         <div className="relative lg:hidden">
           <div className="flex items-center gap-4">
-            <Button variant="normal">Register</Button>
+            {!studentProfile && (
+              <Button
+                variant="normal"
+                onClick={() => {
+                  dispatch(openAuthModal("SIGNUP"));
+                }}
+              >
+                Register
+              </Button>
+            )}
             <button
               onClick={() => {
                 setIsMobileSidebarOpen((prev) => !prev);
@@ -254,13 +277,17 @@ const Navbar = () => {
             >
               <Image src={IMAGES.close} alt="Close" width={24} height={24} />
             </button>
-            <div className="flex  items-center gap-1 py-1">
-              <Image src={IMAGES.profile} alt={""} />
-              <div className="flex flex-col">
-                <span className=" font-bold">John Doe</span>
-                <span>wedontgojim@gmail.com</span>
+            {studentProfile && (
+              <div className="flex  items-center gap-1 py-1">
+                <div className="size-10 rounded-full border border-primary-500 text-primary-500 font-bold flex justify-center items-center">
+                  <p>{studentProfile.full_name[0]}</p>
+                </div>
+                <div className="flex flex-col">
+                  <span className=" font-bold">{studentProfile.full_name}</span>
+                  <span>{studentProfile.email}</span>
+                </div>
               </div>
-            </div>
+            )}
             <hr />
             <ul className=" text-[20px] text-neutral-600 font-semibold">
               {navItems.map((item, index) => (
@@ -273,31 +300,32 @@ const Navbar = () => {
             </ul>
             <hr />
             <ul className="text-[20px] text-neutral-600 font-semibold">
-              {activeTab === "STUDENT"
-                ? pfileItems.map((item, index) => (
-                    <li
-                      key={index}
-                      className="hover:text-primary-500 py-1 font-bold"
-                    >
-                      <Link href={item.href}>
-                        <div>{item.text}</div>
-                      </Link>
-                    </li>
-                  ))
-                : ""}
+              {studentProfile &&
+                pfileItems.map((item, index) => (
+                  <li
+                    key={index}
+                    className="hover:text-primary-500 py-1 font-bold"
+                  >
+                    <Link href={item.href}>
+                      <div>{item.text}</div>
+                    </Link>
+                  </li>
+                ))}
             </ul>
             <div className=" absolute bottom-0 w-full">
               <hr />
-              <button
-                onClick={() => {
-                  mutate();
-                }}
-                className=" text-center text-primary-500 text-xl flex justify-center pl-36 py-4"
-              >
-                <span className=" text-center font-bold">
-                  {isPending ? "Loading..." : "Logout"}
-                </span>
-              </button>
+              {studentProfile && (
+                <button
+                  onClick={() => {
+                    mutate();
+                  }}
+                  className=" text-center text-primary-500 text-xl flex justify-center pl-36 py-4"
+                >
+                  <span className=" text-center font-bold">
+                    {isPending ? "Loading..." : "Logout"}
+                  </span>
+                </button>
+              )}
             </div>
           </div>
         </div>
