6669624d50319867a22cd05b08cdd8ceecada638
MekalaArunKumar16 <mekalaarunkumar2002@gmail.com>
2024-07-22 19:45:53 +0530

    fix:resume skills updated

--- src/app/employer/(home)/dashboard/[jobId]/application/[applicantId]/page.tsx
+++ src/app/employer/(home)/dashboard/[jobId]/application/[applicantId]/page.tsx
@@ -167,95 +167,120 @@ const Profile = ({ params: { applicantId, jobId } }: ProfileProps) => {
                     <div className="flex max-lg:flex-col w-full bg-secondary-200 border border-neutral-100 p-6 max-lg:px-2 max-md:py-4 justify-between rounded-2xl items-center gap-5 mx-16 max-md:mx-4 text-center">
                         <div className='flex gap-4 items-center'>
                             <div>
-                                <Image src={avatar.url} alt="avatar" width={100} height={100} className="rounded-full" />
+                                {avatar ? (
+                                    <Image src={avatar.url} alt="avatar" width={100} height={100} className="rounded-full" />
+                                ) : (
+                                    <div className="w-24 h-24 rounded-full bg-gray-300 flex items-center justify-center">
+                                        No Avatar
+                                    </div>
+                                )}
                             </div>
                             <div className='font-plus-jakarta-sans'>
                                 <div className='flex gap-2'>
                                     <span className='text-neutral-950 text-2xl max-md:text-lg font-600'>{full_name}</span>
                                     <Image src={ICONS.penResume} alt='pen' />
                                 </div>
-                                <span className='text-neutral-600 text-lg max-md:text-xs'>{education[0]?.institutionName}</span>
+                                {education && education.length > 0 ? (
+                                    <span className='text-neutral-600 text-lg max-md:text-xs'>{education[0]?.institutionName}</span>
+                                ) : (
+                                    <span className='text-neutral-600 text-lg max-md:text-xs'>No education details available</span>
+                                )}
                             </div>
                         </div>
-                        <a href={resumes?.url} download target='blank' className='flex items-center justify-center gap-2'>
-                            <Button variant='primary' className="flex items-center gap-[6px] max-w-[150px] justify-center bg-blue-500">
-                                <span className='text-xl'>Download</span>
-                                <Image src={IMAGES.download} alt='download' />
-                            </Button>
-                        </a>
+                        {resumes ? (
+                            <a href={resumes.url} download target='blank' className='flex items-center justify-center gap-2'>
+                                <Button variant='primary' className="flex items-center gap-[6px] max-w-[150px] justify-center bg-blue-500">
+                                    <span className='text-xl'>Download</span>
+                                    <Image src={IMAGES.download} alt='download' />
+                                </Button>
+                            </a>
+                        ) : (
+                            <p>No resume available</p>
+                        )}
                     </div>
                 </div>
                 <div className="pt-2 pb-10 mt-10 font-plus-jakarta-sans">
                     <Card title="Project Details">
-                        {projects.map((project: Project) => (
-                            <div key={project._id} className="flex flex-col max-md:gap-3   p-4 border border-neutral-100 rounded-xl my-2 text-left">
-                                <div className='text-neutral-950 text-3xl max-md:text-sm font-900 mb-5'>
-                                    {project.title}
+                        {projects && projects.length > 0 ? (
+                            projects.map((project: Project) => (
+                                <div key={project._id} className="flex flex-col max-md:gap-3 p-4 border border-neutral-100 rounded-xl my-2 text-left">
+                                    <div className='text-neutral-950 text-3xl max-md:text-sm font-900 mb-5'>
+                                        {project.title}
+                                    </div>
+                                    <div className='text-neutral-600 text-lg max-md:text-xs mb-5'>
+                                        <li className='list-disc'>
+                                            {project.description}
+                                        </li>
+                                    </div>
+                                    <div className='flex gap-4 items-center'>
+                                        <Link href={project.link} target="_blank" className='text-blue-500 text-lg max-md:text-xs'>
+                                            View Project
+                                        </Link>
+                                    </div>
                                 </div>
-                                <div className='text-neutral-600 text-lg max-md:text-xs  mb-5'>
-                                    <li className=' list-disc'>
-                                        {project.description}
-                                    </li>
-                                </div>
-                                <div className='flex gap-4 items-center'>
-                                    <Link href={project.link} target="_blank" className='text-blue-500 text-lg max-md:text-xs'>
-                                        View Project
-                                    </Link>
-                                </div>
-                            </div>
-                        ))}
+                            ))
+                        ) : (
+                            <p>No project details available</p>
+                        )}
                     </Card>
                 </div>
                 <div className="pt-2 pb-10 font-plus-jakarta-sans">
                     <Card title="Experience">
-                        {experience.map((exp: Experience) => (
-                            <div key={exp._id} className="flex flex-col  p-4 border border-neutral-100 rounded-xl my-2 text-left">
-                                <div className='text-neutral-950 text-3xl max-md:text-sm font-600'>
-                                    {exp.title} @ {exp.company}
+                        {experience && experience.length > 0 ? (
+                            experience.map((exp: Experience) => (
+                                <div key={exp._id} className="flex flex-col p-4 border border-neutral-100 rounded-xl my-2 text-left">
+                                    <div className='text-neutral-950 text-3xl max-md:text-sm font-600'>
+                                        {exp.title} @ {exp.company}
+                                    </div>
+                                    <div className='text-neutral-600 text-lg max-md:text-xs'>
+                                        {exp.startDate} - {exp.endDate}
+                                    </div>
+                                    <div className='text-neutral-600 text-lg max-md:text-xs mt-5'>
+                                        {exp.description}
+                                    </div>
                                 </div>
-                                <div className='text-neutral-600 text-lg max-md:text-xs'>
-                                    {exp.startDate} - {exp.endDate}
-                                </div>
-                                <div className='text-neutral-600 text-lg max-md:text-xs mt-5'>
-                                    {exp.description}
-                                </div>
-                            </div>
-                        ))}
+                            ))
+                        ) : (
+                            <p>No experience details available</p>
+                        )}
                     </Card>
                 </div>
                 <div className="pt-2 pb-10 font-plus-jakarta-sans">
                     <Card title="Certifications">
-                        {certifications.map((cert: Certification) => (
-                            <div key={cert._id} className="flex flex-col  p-4 border border-neutral-100 rounded-xl my-2 text-left">
-                                <div className='text-neutral-950 text-lg max-md:text-sm font-600'>
-                                    {cert.name}
-                                </div>
-                                <div className='text-neutral-600 text-lg max-md:text-xs'>
-                                    Issued by: {cert.issuingOrganization}
-                                </div>
-                                <div className='flex gap-3'>
-                                    <div className='text-neutral-600 text-lg max-md:text-xs'>
-                                        Issue Date: {cert.issueDate} 
+                        {certifications && certifications.length > 0 ? (
+                            certifications.map((cert: Certification) => (
+                                <div key={cert._id} className="flex flex-col p-4 border border-neutral-100 rounded-xl my-2 text-left">
+                                    <div className='text-neutral-950 text-3xl max-md:text-sm font-600'>
+                                        {cert.name} @ {cert.issuingOrganization}
                                     </div>
-                                    |
                                     <div className='text-neutral-600 text-lg max-md:text-xs'>
-                                        Expiration Date: {cert.expirationDate}
+                                        {cert.issueDate} - {cert.expirationDate}
+                                    </div>
+                                    <div className='text-neutral-600 text-lg max-md:text-xs mt-5'>
+                                        <a href={cert.credentialURL} target="_blank" className='text-blue-500 text-lg max-md:text-xs'>
+                                            View Credential
+                                        </a>
                                     </div>
                                 </div>
-                                <div className='text-blue-500 text-lg max-md:text-xs mt-5'>
-                                    <a href={cert.credentialURL} target="_blank" rel="noopener noreferrer">View Certificate</a>
-                                </div>
-                            </div>
-                        ))}
+                            ))
+                        ) : (
+                            <p>No certifications available</p>
+                        )}
                     </Card>
                 </div>
                 <div className="pt-2 pb-10 font-plus-jakarta-sans">
                     <Card title="Skills">
-                        <div className="flex gap-2 flex-wrap ">
-                            {skills.map((skill: string) => (
-                                <Chip key={skill} variant='close' children={skill}/>
-                            ))}
-                        </div>
+                        {skills && skills.length > 0 ? (
+                            <div className="flex flex-wrap gap-2">
+                                {skills.map((skill: string, index: number) => (
+                                    <Chip key={index} children={skill} variant='close' />
+                                ))}
+                            </div>
+                        ) : (
+                            <div className='flex  justify-center text-center'>
+                                    <p className='text-center'>No Skills links available</p>
+                                </div>
+                        )}
                     </Card>
                 </div>
                 <div className="pt-2 pb-10 font-plus-jakarta-sans">
@@ -264,11 +289,13 @@ const Profile = ({ params: { applicantId, jobId } }: ProfileProps) => {
                             {socialLinks ? (
                                 Object.keys(socialLinks).map((key) => (
                                     <a key={key} href={socialLinks[key]} target="_blank" rel="noopener noreferrer" className='text-blue-500'>
-                                         <Chip key={key} variant='close' children={key}/>
+                                        <Chip key={key} variant='close' children={key} />
                                     </a>
                                 ))
                             ) : (
-                                <p>No social links available</p>
+                                <div className='flex ml-[480px] justify-center text-center'>
+                                    <p className='text-center'>No social links available</p>
+                                </div>
                             )}
                         </div>
                     </Card>
