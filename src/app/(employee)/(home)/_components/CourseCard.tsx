0c79b5253121045323db163c1284c90349806a8b
RAHUL <rahulsd380@gmail.com>
2025-04-09 00:40:56 +0600

    Added see more and less btn on course card

--- src/app/(employee)/(home)/_components/CourseCard.tsx
+++ src/app/(employee)/(home)/_components/CourseCard.tsx
@@ -1,9 +1,26 @@
+"use client";
 import { ICONS, IMAGES } from "@/assets";
 import Image from "next/image";
+import { useState } from "react";
 
 const CourseCard = () => {
+  const courseIncludes = [
+    "Adobe Illustrator",
+    "Adobe Illustrator",
+    "Adobe Illustrator",
+    "Adobe Illustrator",
+    "Adobe Illustrator",
+  ];
+
+  const [showAll, setShowAll] = useState<boolean>(false);
+
+  const visibleItems = showAll ? courseIncludes : courseIncludes.slice(0, 2);
   return (
-    <div className="bg-white p-6 w-full max-w-[446px] rounded-3xl border border-neutral-300 shadow-job-card-shadow font-plus-jakarta-sans flex flex-col gap-6">
+    <div
+      className={`bg-white p-6 w-full max-w-[446px] rounded-3xl border border-neutral-300 shadow-job-card-shadow font-plus-jakarta-sans flex flex-col gap-6 ${
+        showAll ? "h-[auto]" : "h-[710px]"
+      }`}
+    >
       <Image src={IMAGES.courseImg} alt="" className="" />
       {/* Course Name */}
       <h1 className="text-neutral-900 text-[28px] font-800 ">
@@ -14,16 +31,25 @@ const CourseCard = () => {
       <div>
         <h2 className="text-neutral-980 text-2xl font-600 ">This Includes:</h2>
         <div className="flex flex-col gap-3 mt-4">
-          <div className="flex items-center gap-[7px]">
-            <Image
-              src={ICONS.rightArrowCourse}
-              alt="right-pointer-arrow-icon"
-              className="size-[21px]"
-            />
-            <p className="text-neutral-990 text-[21px] capatalize">
-              Adobe Illustrator
-            </p>
-          </div>
+          {visibleItems.map((item, index) => (
+            <div key={index} className="flex items-center gap-[7px]">
+              <Image
+                src={ICONS.rightArrowCourse}
+                alt="right-pointer-arrow-icon"
+                className="size-[21px]"
+              />
+              <p className="text-neutral-990 text-[21px] capitalize">{item}</p>
+            </div>
+          ))}
+
+          {courseIncludes.length > 2 && (
+            <button
+              onClick={() => setShowAll(!showAll)}
+              className="text-primary-500 text-xs underline w-fit"
+            >
+              {showAll ? "See Less" : "See More"}
+            </button>
+          )}
         </div>
       </div>
 
