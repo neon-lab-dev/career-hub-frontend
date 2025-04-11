ebb74997e71d0121460f19ae89557fab56050b0a
RAHUL <rahulsd380@gmail.com>
2025-04-11 22:00:13 +0600

    Added reusable modal

---
+++ src/components/Reusable/Modal/Modal.tsx
@@ -0,0 +1,39 @@
+import { IMAGES } from "@/assets";
+import Image from "next/image";
+import { ReactNode } from "react";
+
+type TModal = {
+heading : string;
+  children: ReactNode;
+  isModalOpen: boolean;
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+const Modal: React.FC<TModal> = ({ heading, children, isModalOpen, setIsModalOpen }) => {
+  return (
+    <div
+      className={`${
+        isModalOpen ? " visible" : " invisible"
+      } w-full h-screen fixed top-0 left-0 z-[200000000] bg-[#0000002a] flex items-center justify-center transition-all duration-300 font-plus-jakarta-sans`}
+    >
+      <div
+        className={`${
+          isModalOpen ? " scale-[1] opacity-100" : " scale-[0] opacity-0"
+        } max-w-[673px] w-full bg-white rounded-[28px] p-8 transition-all duration-300`}
+      >
+        <div className="w-full flex items-center justify-between">
+          <h1 className="text-neutral-700 font-500">{heading}</h1>
+          <Image
+            src={IMAGES.close}
+            alt="cross-icon"
+            className="p-2 size-10 hover:bg-[#e7e7e7] rounded-full transition-all duration-300 cursor-pointer"
+            onClick={() => setIsModalOpen(false)}
+          />
+        </div>
+
+        <div className="w-full flex justify-center flex-col">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/components/Reusable/RegistrationFormModal/RegistrationFormModal.tsx
+++ src/components/Reusable/RegistrationFormModal/RegistrationFormModal.tsx
@@ -6,6 +6,7 @@ import TextInput from "../TextInput/TextInput";
 import { FieldErrors, UseFormRegister } from "react-hook-form";
 import DropdownInput from "../DopdownInput/DropdownInput";
 import Button from "@/components/Button";
+import Modal from "../Modal/Modal";
 
 type TRegistrationFormModalProps = {
   isModalOpen: boolean;
@@ -20,8 +21,14 @@ const RegistrationFormModal: React.FC<TRegistrationFormModalProps> = ({
   register,
   errors,
 }) => {
-  const [selectedDesignation, setSelectedDesignation] = useState<string>("Medical");
-  const designationTypes = ["Medical", "Paramedical", "Paramedical Diploma", "Other"]
+  const [selectedDesignation, setSelectedDesignation] =
+    useState<string>("Medical");
+  const designationTypes = [
+    "Medical",
+    "Paramedical",
+    "Paramedical Diploma",
+    "Other",
+  ];
   const medicalCourses = [
     "Ayurvedic Medicine and Surgery",
     "Dental Surgery",
@@ -71,140 +78,119 @@ const RegistrationFormModal: React.FC<TRegistrationFormModalProps> = ({
     "X-Ray Technology",
   ];
 
-  const options = selectedDesignation === "Medical" ?
-  medicalCourses
-  :
-  selectedDesignation === "Paramedical" ?
-  paramedicalCourses
-  :
-  selectedDesignation === "Paramedical Diploma" ?
-  paramedicalDiplomaCourses
-  :
-  []
+  const options =
+    selectedDesignation === "Medical"
+      ? medicalCourses
+      : selectedDesignation === "Paramedical"
+      ? paramedicalCourses
+      : selectedDesignation === "Paramedical Diploma"
+      ? paramedicalDiplomaCourses
+      : [];
 
   return (
-    <div
-      className={`${
-        isModalOpen ? " visible" : " invisible"
-      } w-full h-screen fixed top-0 left-0 z-[200000000] bg-[#0000002a] flex items-center justify-center transition-all duration-300 font-plus-jakarta-sans`}
+    <Modal
+      heading="Designation"
+      isModalOpen={isModalOpen}
+      setIsModalOpen={setIsModalOpen}
     >
-      <div
-        className={`${
-          isModalOpen ? " scale-[1] opacity-100" : " scale-[0] opacity-0"
-        } max-w-[673px] w-full bg-white rounded-[28px] p-8 transition-all duration-300`}
-      >
-        <div className="w-full flex items-center justify-between">
-          <h1 className="text-neutral-700 font-500">Designation</h1>
-          <Image
-            src={IMAGES.close}
-            alt="cross-icon"
-            className="p-2 size-10 hover:bg-[#e7e7e7] rounded-full transition-all duration-300 cursor-pointer"
-            onClick={() => setIsModalOpen(false)}
-          />
-        </div>
-
-        <div className="w-full flex justify-center flex-col mt-3">
-          {/* Radio inputs */}
-          <div className="flex items-center gap-6">
-            {designationTypes?.map((designation) => (
-              <button
-                key={designation}
-                type="button"
-                onClick={() => setSelectedDesignation(designation)}
-                className="flex items-center gap-1 cursor-pointer"
-              >
-                {selectedDesignation === designation ? (
-                  <Image
-                    src={ICONS.radioButtonChecked}
-                    alt="radio-icon"
-                    className="size-5"
-                  />
-                ) : (
-                  <Image
-                    src={ICONS.radioButtonUnchecked}
-                    alt="radio-icon"
-                    className="size-5"
-                  />
-                )}
-                <h1
-                  className={`${
-                    selectedDesignation === designation
-                      ? "text-neutral-900"
-                      : "text-neutral-500"
-                  } font-500`}
-                >
-                  {designation}
-                </h1>
-              </button>
-            ))}
-          </div>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
-            <TextInput
-              label="Institute Name"
-              placeholder="eg.,  Meenakshi college of engineering"
-              error={errors.fullName}
-              {...register("fullName")}
-              isRequired={false}
-            />
-            <TextInput
-              label="City & State"
-              placeholder="eg., Mumbai"
-              error={errors.fullName}
-              {...register("fullName")}
-              isRequired={false}
-            />
-            {
-              selectedDesignation !== "Other" ?
-              <DropdownInput
-              label="Course"
-              {...register("occupation")}
-              error={errors.occupation}
-              options={options}
-              isRequired={false}
-            />
-            :
-            <TextInput
-              label="Course"
-              placeholder="ex: Full stack web development"
-              error={errors.fullName}
-              {...register("fullName")}
-              isRequired={false}
-            />
-            }
-            <TextInput
-              label="Grade / Percentage"
-              placeholder="eg., 3.85/4"
-              error={errors.fullName}
-              {...register("fullName")}
-              isRequired={false}
-            />
-            <TextInput
-              label="From"
-              type="date"
-              error={errors.fullName}
-              {...register("fullName")}
-              isRequired={false}
-            />
-            <TextInput
-              label="To"
-              type="date"
-              error={errors.fullName}
-              {...register("fullName")}
-              isRequired={false}
-            />
-          </div>
-          <div className="flex items-center gap-3 mt-6">
-            <Button variant="natural" className="px-6 py-3">
-              Cancel
-            </Button>
-            <Button type="submit" variant="normal" className="px-6 py-3">
-              Add
-            </Button>
-          </div>
-        </div>
+      {/* Radio inputs */}
+      <div className="flex items-center gap-6">
+        {designationTypes?.map((designation) => (
+          <button
+            key={designation}
+            type="button"
+            onClick={() => setSelectedDesignation(designation)}
+            className="flex items-center gap-1 cursor-pointer"
+          >
+            {selectedDesignation === designation ? (
+              <Image
+                src={ICONS.radioButtonChecked}
+                alt="radio-icon"
+                className="size-5"
+              />
+            ) : (
+              <Image
+                src={ICONS.radioButtonUnchecked}
+                alt="radio-icon"
+                className="size-5"
+              />
+            )}
+            <h1
+              className={`${
+                selectedDesignation === designation
+                  ? "text-neutral-900"
+                  : "text-neutral-500"
+              } font-500`}
+            >
+              {designation}
+            </h1>
+          </button>
+        ))}
       </div>
-    </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
+        <TextInput
+          label="Institute Name"
+          placeholder="eg.,  Meenakshi college of engineering"
+          error={errors.fullName}
+          {...register("fullName")}
+          isRequired={false}
+        />
+        <TextInput
+          label="City & State"
+          placeholder="eg., Mumbai"
+          error={errors.fullName}
+          {...register("fullName")}
+          isRequired={false}
+        />
+        {selectedDesignation !== "Other" ? (
+          <DropdownInput
+            label="Course"
+            {...register("occupation")}
+            error={errors.occupation}
+            options={options}
+            isRequired={false}
+          />
+        ) : (
+          <TextInput
+            label="Course"
+            placeholder="ex: Full stack web development"
+            error={errors.fullName}
+            {...register("fullName")}
+            isRequired={false}
+          />
+        )}
+        <TextInput
+          label="Grade / Percentage"
+          placeholder="eg., 3.85/4"
+          error={errors.fullName}
+          {...register("fullName")}
+          isRequired={false}
+        />
+        <TextInput
+          label="From"
+          type="date"
+          error={errors.fullName}
+          {...register("fullName")}
+          isRequired={false}
+        />
+        <TextInput
+          label="To"
+          type="date"
+          error={errors.fullName}
+          {...register("fullName")}
+          isRequired={false}
+        />
+      </div>
+      <div className="flex items-center gap-3 mt-6">
+        <Button variant="natural" className="px-6 py-3">
+          Cancel
+        </Button>
+        <Button type="submit" variant="normal" className="px-6 py-3">
+          Add
+        </Button>
+      </div>
+    </Modal>
   );
 };
 
